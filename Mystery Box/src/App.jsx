9dc63b3fc3162040319f7243a4a43b7de46c7bdc
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-06-05 23:22:42 +0530

    Mystery Box

---
+++ Mystery Box/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+
+  <div className="App">
+  
+    <div className="box top"></div>
+    <div className="box left"></div>
+    <div className="box right"></div>
+    <div className="box bottom"></div>
+  
+  </div>
+);
