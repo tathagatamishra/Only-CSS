b12ce5f313de16daf828052af75ee0c9a173fff1
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-06-13 21:36:26 +0530

    done

---
+++ Snake/README.md
@@ -0,0 +1,73 @@
+Certainly! Here's an example of a `README.md` file for the Snake Game:
+
+# Snake Game
+
+This is a simple Snake game implemented using React. The player controls a snake on a game board and can move it in four directions: up, right, down, and left. The game can be played using keyboard arrow keys or by clicking on the control buttons displayed on the screen.
+
+![Snake Game Screenshot](./screenshot.png)
+
+## Technologies Used
+
+- React
+- CSS
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/your-username/snake-game.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd snake-game
+```
+
+3. Install the dependencies:
+
+```bash
+npm install
+```
+
+4. Start the development server:
+
+```bash
+npm start
+```
+
+5. Open your browser and visit `http://localhost:3000` to play the game.
+
+## How to Play
+
+- Use the arrow keys on your keyboard to move the snake in the corresponding directions.
+- Alternatively, you can use the control buttons displayed on the screen: ▲ for up, ► for right, ▼ for down, and ◄ for left.
+- The goal of the game is to navigate the snake to eat the food that appears on the board and grow in length.
+- Be careful not to collide with the walls or the snake's own body, as it will result in game over.
+- The game will keep track of your score, which increases as the snake eats food.
+
+## Customization
+
+- The initial position of the snake on the game board can be adjusted by modifying the `x` and `y` values in the `useState` hook of the `App` component.
+
+```jsx
+const [snake, setSnake] = useState({
+  x: 2200, // Adjust the x-coordinate as desired
+  y: 1000, // Adjust the y-coordinate as desired
+});
+```
+
+- You can customize the appearance of the game board and the snake by modifying the CSS styles defined in the `App.css` file.
+
+## Acknowledgments
+
+This Snake game is a basic implementation and serves as a starting point for building more advanced features. Feel free to enhance and expand upon it to create your own unique Snake game!
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+Feel free to update and customize the `README.md` file to match your specific project structure and requirements. Include relevant sections such as gameplay instructions, customization options, acknowledgments, and license information. Additionally, you can add screenshots or GIFs to showcase the game in action.
--- Snake/src/App.jsx
+++ Snake/src/App.jsx
@@ -1,30 +1,86 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [snake, setSnake] = useState({});
+  const [snake, setSnake] = useState({
+    x: 0,
+    y: 0,
+  });
 
-  let x = 100
-  function control() {
-    setSnake({
-      transform: 'translateX(50%)',
-      transform: `translateY(${x}%)`
-    })
-    x=x+100
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
+  function handleKeyPress(event) {
+    const key = event.key.toLowerCase();
+    if (["w", "arrowup"].includes(key)) {
+      control("a");
+    } else if (["d", "arrowright"].includes(key)) {
+      control("b");
+    } else if (["s", "arrowdown"].includes(key)) {
+      control("c");
+    } else if (["a", "arrowleft"].includes(key)) {
+      control("d");
+    }
+  }
+
+  function control(key) {
+    switch (key) {
+      case "a":
+        setSnake((prevSnake) => ({
+          ...prevSnake,
+          y: prevSnake.y - 100,
+        }));
+        break;
+      case "b":
+        setSnake((prevSnake) => ({
+          ...prevSnake,
+          x: prevSnake.x + 100,
+        }));
+        break;
+      case "c":
+        setSnake((prevSnake) => ({
+          ...prevSnake,
+          y: prevSnake.y + 100,
+        }));
+        break;
+      case "d":
+        setSnake((prevSnake) => ({
+          ...prevSnake,
+          x: prevSnake.x - 100,
+        }));
+        break;
+      default:
+        break;
+    }
   }
 
   return (
     <div className="App">
-      <div className='snake' style={snake}></div>
+      <div
+        className="snake"
+        style={{ transform: `translate(${snake.x}%, ${snake.y}%)` }}
+      ></div>
 
-      <section className='control'>
-        <button className='a' onClick={control}>a</button>
-        <button className='b'>b</button>
-        <button className='c'>c</button>
-        <button className='d'>d</button>
+      <section className="control">
+        <button className="a" onClick={() => control("a")}>
+          &#9651;
+        </button>
+        <button className="b" onClick={() => control("b")}>
+          &#9711;
+        </button>
+        <button className="c" onClick={() => control("c")}>
+          &#9587;
+        </button>
+        <button className="d" onClick={() => control("d")}>
+          &#9744;
+        </button>
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
